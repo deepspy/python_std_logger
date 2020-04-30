23dbc83d9912e64837d9a61d3e94eeefd0882b4c
deepspy <pavel.deepspy@gmail.com>
2020-04-30 03:39:30 +0300

    Added git ignore.

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ print("This text will be logged.")
 import sys
 
 print('Log filename is', sys.stdout.filename)
-print("Lets move the log file to a new location 'logdir'")
-sys.stdout.move_file('logdir')
+print("Lets move the log file to a new location 'example_logdir'")
+sys.stdout.move_file('example_logdir')
 print("From now on, the log file is named", sys.stdout.filename)
 
 print("Have fun using the python std logger!")
